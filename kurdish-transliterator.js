5c6acdc2e4a08dd74e54c4b904775dd4bd875be5
Payam <payambapiri.97@gmail.com>
2021-08-21 02:00:03 +0430

    Clean-up codes.

--- kurdish-transliterator.js
+++ kurdish-transliterator.js
@@ -1,33 +1,26 @@
-/*
-  Copyright (C) 2020 allekok.
-  Author: Payam <payambapiri.97@gmail.com>
-  License: MIT License
-*/
-function transliterate_ar2lat (str) {
+function transliterate_ar2lat(str) {
 	return apply_to_words(str, w => ar2lat(w))
 }
-
-function transliterate_ar2per (str) {
+function transliterate_ar2per(str) {
 	return apply_to_words(str, w => ar2per(w))
 }
-
-function ar2IL (s) {
-	const bizroke = 'i';
-	const v = "ەeێêۆoاaiuîûأإآ";
-	const n = "قwرڕتyئحعپسشدفگغهژکلڵزخجچڤبنمڎصۊۉثذضظةطؤ";
+function ar2IL(s) {
+	const bizroke = 'i'
+	const v = "ەeێêۆoاaiuîûأإآ"
+	const n = "قwرڕتyئحعپسشدفگغهژکلڵزخجچڤبنمڎصۊۉثذضظةطؤ"
 	const before = [["عیوونی", "عiیوونی"],
 			["ئارەزوی", "ئارەزuی"],
 			["ئارزوی", "ئارزuی"],
 			["^([رڕ])وی$", "$1uی"],
 			["^لە([رڕ])وی$", "لە$1uی"],
-			["هاووڵا", "هاwwڵا"]];
+			["هاووڵا", "هاwwڵا"]]
 	const after = [["ûyyî$", "ûyîy"],
-		       [`^([مس])ە([رح])u$`, "$1ە$2w"]];
+		       [`^([مس])ە([رح])u$`, "$1ە$2w"]]
 	const notsure = [["وو", "û", "uw", "wu", "ww"],
 			 ["یی", "îy", "îy", "yî", "yy"],
 			 ["ی", "î", "y"],
-			 ["و", "u", "w"]];
-	function determine_notsure (R, str) {
+			 ["و", "u", "w"]]
+	function determine_notsure(R, str) {
 		let pos = R[0],
 		    ch = R[1][0],
 		    ch_len = ch.length,
@@ -36,28 +29,28 @@ function ar2IL (s) {
 		    next_ch_2 = L(str, pos+ch_len+1),
 		    next_v = is_(next_ch, v),
 		    next_v_2 = is_(next_ch_2, v),
-		    i = 1; // v
-		if(prev_ch == '‌') prev_ch = L(str, pos-2);
-		let prev_v = is_(prev_ch, v);
+		    i = 1 /* v */
+		if(prev_ch == '‌') prev_ch = L(str, pos-2)
+		let prev_v = is_(prev_ch, v)
 		
 		if(is_(str, ["وو","یی","ی","و"]));
 		else if(ch_len == 2) {
 			if(prev_v && !next_v_2 &&
-			   (next_v || is_(next_ch,'یو'))) i = 4;
-			else if(pos == 0 || prev_v) i = 3;
-			else if(next_v) i = 2;
+			   (next_v || is_(next_ch,'یو'))) i = 4
+			else if(pos == 0 || prev_v) i = 3
+			else if(next_v) i = 2
 		}
 		else if(pos == 0 || prev_v || next_v ||
 			(prev_ch != 'y' && ch == 'و' &&
-			 next_ch == 'ی' && !next_v_2)) i = 2;
-		return i;
+			 next_ch == 'ی' && !next_v_2)) i = 2
+		return i
 	}
-	return add_bizroke(replace_sure(replace_notsure(replace_sure(
-		standardizing(s), before), notsure, determine_notsure),
-					after), n, v, bizroke);
+	return add_bizroke(replace_sure(replace_notsure(
+		replace_sure(standardizing(s), before),
+		notsure, determine_notsure), after),
+			   n, v, bizroke)
 }
-
-function ar2lat (s) {
+function ar2lat(s) {
 	const sure = [["أ","ئە"],
 		      ["إ","ئی"],
 		      ["آ","ئا"],
@@ -110,17 +103,16 @@ function ar2lat (s) {
 		      ["٩|۹", "9"],
 		      ["،", ","],
 		      ["؛", ";"],
-		      ["؟", "?"]];
+		      ["؟", "?"]]
 	/* Tashdid */
-	function remove_tashdid (s, tashdid="\u{651}") {
+	function remove_tashdid(s, tashdid="\u{651}") {
 		return replace_sure(s, [
 			[`(.)([َُِ])${tashdid}`, "$1$1$2"],
-			[`(.)${tashdid}`, "$1$1"]]);
+			[`(.)${tashdid}`, "$1$1"]])
 	}
-	return replace_sure(remove_tashdid(ar2IL(s)), sure);
+	return replace_sure(remove_tashdid(ar2IL(s)), sure)
 }
-
-function ar2per (s) {
+function ar2per(s) {
 	const sure = [["wu", "و\u{64F}"],
 		      ["û", "و\u{64F}"],
 		      ["ە", "\u{64E}"],
@@ -140,31 +132,34 @@ function ar2per (s) {
 		      ["٦", "۶"],
 		      ["٧", "۷"],
 		      ["٨", "۸"],
-		      ["٩", "۹"]];
-	const n = "قرڕتئحعپسشدفگغهژکلڵزخجچڤبنمڎصۊۉثذضظةطؤ";
-	const v = "ەێۆاuûîiأإآ";
+		      ["٩", "۹"]]
+	const n = "قرڕتئحعپسشدفگغهژکلڵزخجچڤبنمڎصۊۉثذضظةطؤ"
+	const v = "ەێۆاuûîiأإآ"
 	/* Tashdid */
-	function add_tashdid (str, n, v, tashdid="\u{651}") {
-		for (let i = 0; i < str.length-2; i++) {
+	function add_tashdid(str, n, v, tashdid="\u{651}") {
+		for(let i = 0; i < str.length-2; i++) {
 			if(str[i] == str[i+1] && is_(str[i], n) &&
 			   is_(str[i-1], v) && is_(str[i+2], v))
-				str = str_replace_pos(
-					str[i]+str[i], str[i]+tashdid, str, i);
+				str = str_replace_pos(str[i]+str[i],
+						      str[i]+tashdid,
+						      str,
+						      i)
 		}
-		return str;
+		return str
 	}
 	/* Beginning 'Hemze' */
-	function determine_hemze (s) {
+	function determine_hemze(s) {
 		if(s.startsWith("ئ"))
-			return determine_hemze(str_replace_pos("ئ", "ا", s, 0));
+			return determine_hemze(str_replace_pos(
+				"ئ", "ا", s, 0))
 		else if(s.startsWith("اا"))
-			return str_replace_pos("اا", "آ", s, 0);
-		return s;
+			return str_replace_pos("اا", "آ", s, 0)
+		return s
 	}
-	return replace_sure(add_tashdid(determine_hemze(ar2IL(s)), n, v), sure);
+	return replace_sure(add_tashdid(
+		determine_hemze(ar2IL(s)), n, v), sure)
 }
-
-function standardizing (str) {
+function standardizing(str) {
 	return replace_sure(str, [
 		["ـ",""],
 		["‌+","‌"],
@@ -189,89 +184,80 @@ function standardizing (str) {
 		["ﻗ","ق"],
 		["ﺋ","ئ"],
 		["ﺱ|ﺳ","س"],
-		["ﺸ|ﺷ","ش"]]);
+		["ﺸ|ﺷ","ش"]])
 }
-
-function add_bizroke (str, n, v, bizroke="") {
+function add_bizroke(str, n, v, bizroke="") {
 	/* I don't know the exact specification for this procedure. */
-	function is_n (ch) { return is_(ch, n) }
-	const L1 = L(str, 0);
-	const L2 = L(str, 1);
+	function is_n(ch) { return is_(ch, n) }
+	const L1 = L(str, 0)
+	const L2 = L(str, 1)
 	if(is_n(L1) && (!L2 || is_n(L2)))
-		str = str_replace_pos("", bizroke, str, 1);
-	return str;
+		str = str_replace_pos("", bizroke, str, 1)
+	return str
 }
-
-function replace_sure (str, sure, f=0, t=1) {
+function replace_sure(str, sure, f=0, t=1) {
 	for(const o of sure)
-		str = str.replace(new RegExp(o[f],"g"), o[t]);
-	return str;
+		str = str.replace(new RegExp(o[f], "g"), o[t])
+	return str
 }
-
-function replace_notsure (str, notsure, determine_fun, i=0) {
-	let R;
+function replace_notsure(str, notsure, determine_fun, i=0) {
+	let R
 	while(false !== (R = assoc_first(str, notsure, i))) {
-		const j = determine_fun(R, str);
-		str = str_replace_pos(R[1][i], R[1][j], str, R[0]);
+		const j = determine_fun(R, str)
+		str = str_replace_pos(R[1][i], R[1][j], str, R[0])
 	}
-	return str;
+	return str
 }
-
-function assoc_first (str, arr, i=0, off=0) {
-	const str_len = str.length;
+function assoc_first(str, arr, i=0, off=0) {
+	const str_len = str.length
 	for(let j = off; j < str_len; j++)
 		for(const o of arr)
 			if(o[i] == str.substr(j, o[i].length))
-				return [j, o];
-	return false;
+				return [j, o]
+	return false
 }
-
-function L (str, pos, len=1) {
-	return str.substr(pos, len);
+function L(str, pos, len=1) {
+	return str.substr(pos, len)
 }
-
-function is_ (c, x) {
-	if(c && x.indexOf(c) !== -1) return true;
-	return false;
+function is_(c, x) {
+	if(c && x.indexOf(c) !== -1) return true
+	return false
 }
-
-function str_replace_pos (from, to, str, pos) {
+function str_replace_pos(from, to, str, pos) {
 	return str.substr(0, pos) + to +
-		str.substr(pos + from.length);
+		str.substr(pos + from.length)
 }
-
-function apply_to_words (str, fun) {
-	let include = "«»`1234567890-=~!@#$%^&*()_+[]{}\\|;:'\",./<>?؛،؟١٢٣٤٥٦٧٨٩٠ \n\t\rABCDEFGHIJKLMNOPQRSTUVWXYZ",
-	    i = 0, new_str = '';
+function apply_to_words(str, fun) {
+	let include = "«»`1234567890-=~!@#$%^&*()_+[]{}\\|;:'\"," +
+	    "./<>?؛،؟١٢٣٤٥٦٧٨٩٠ \n\t\rABCDEFGHIJKLMNOPQRSTUVWXYZ",
+	    i = 0, new_str = ''
 	while(str[i] !== undefined) {
-		let token = '';
+		let token = ''
 		while(str[i] !== undefined &&
 		      include.indexOf(str[i].toUpperCase()) === -1)
-			token += str[i++];
+			token += str[i++]
 		if(!token)
-			while(str[i] !== undefined &&
-			      include.indexOf(str[i].toUpperCase()) !== -1)
-				token += str[i++];
-		new_str += fun(token);
+			while(str[i] !== undefined && include.indexOf(
+				str[i].toUpperCase()) !== -1)
+				token += str[i++]
+		new_str += fun(token)
 	}
-	return new_str;
+	return new_str
 }
-
-function apply_to_text (el, proc) {
-	let html = '';
+function apply_to_text(el, proc) {
+	let html = ''
 	for(const o of el.childNodes) {
 		if(o.nodeName == '#text') {
 			if(o.parentElement.className.
 			   indexOf('material-icons') === -1)
-				html += proc(o.data);
+				html += proc(o.data)
 			else
-				html += o.data;
-		}
-		else {
-			apply_to_text(o, proc);
+				html += o.data
+		} else {
+			apply_to_text(o, proc)
 			if(o.outerHTML !== undefined)
-				html += o.outerHTML;
+				html += o.outerHTML
 		}
 	}
-	el.innerHTML = html;
+	el.innerHTML = html
 }
